ffb2d6bc60b780cf569324a7717f7b9b76c50ae8
occn8 <ochenwayward@gmail.com>
2021-07-21 21:00:00 +0300

    settings

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class Home extends StatefulWidget {
   Home({Key? key}) : super(key: key);
@@ -10,10 +11,152 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-       body: Container(
-         
-       ),
+    var _height = MediaQuery.of(context).size.height * 0.2;
+    var _width = MediaQuery.of(context).size.width * 0.5;
+
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: SystemUiOverlayStyle.dark,
+      child: Scaffold(
+        body: Container(
+          padding: EdgeInsets.symmetric(horizontal: 8, vertical: 5),
+          child: ListView(
+            children: [
+              Container(
+                padding: EdgeInsets.symmetric(vertical: 4, horizontal: 5),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Builder(
+                      builder: (context) => InkWell(
+                        splashColor: Theme.of(context).accentColor,
+                        borderRadius: BorderRadius.circular(10),
+                        onTap: () => Scaffold.of(context).openDrawer(),
+                        child: Container(
+                          padding: new EdgeInsets.all(8),
+                          decoration: BoxDecoration(
+                              // color: Theme.of(context).cardColor,
+                              borderRadius: BorderRadius.circular(10)),
+                          child: new Icon(Icons.menu,
+                              size: 25,
+                              color: Theme.of(context).primaryColorDark),
+                        ),
+                      ),
+                    ),
+                    RichText(
+                      text: TextSpan(
+                          children: [
+                            TextSpan(
+                                text: 'Fit',
+                                style: TextStyle(
+                                    fontSize: 24,
+                                    fontWeight: FontWeight.w800,
+                                    color: Colors.orange[800])),
+                            TextSpan(text: 'Me '),
+                          ],
+                          style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.w500,
+                              color: Colors.blueGrey)),
+                    ),
+                    
+                    Builder(
+                      builder: (context) => InkWell(
+                        splashColor: Theme.of(context).accentColor,
+                        borderRadius: BorderRadius.circular(10),
+                        onTap: () {},
+                        child: Container(
+                          padding: new EdgeInsets.all(8),
+                          decoration: BoxDecoration(
+                              // color: Theme.of(context).cardColor,
+                              borderRadius: BorderRadius.circular(10)),
+                          child: new Icon(Icons.settings,
+                              size: 25,
+                              color: Theme.of(context).primaryColorDark),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(vertical: 5, horizontal: 8),
+                child: Text(
+                  'Work Outs',
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+                ),
+              ),
+              WorkOutItem(
+                height: _height,
+                width: _width,
+                imgUrl: 'assets/images/healthy_lifestyle.png',
+              ),
+              WorkOutItem(
+                height: _height,
+                width: _width,
+                imgUrl: 'assets/images/healthy_lifestyle.png',
+              ),
+              WorkOutItem(
+                height: _height,
+                width: _width,
+                imgUrl: 'assets/images/healthy_lifestyle.png',
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class WorkOutItem extends StatelessWidget {
+  const WorkOutItem({
+    Key? key,
+    required double height,
+    required double width,
+    required this.imgUrl,
+  })   : _height = height,
+        _width = width,
+        super(key: key);
+
+  final double _height;
+  final double _width;
+  final String imgUrl;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 150,
+      padding: EdgeInsets.all(5),
+      margin: EdgeInsets.all(5),
+      decoration: BoxDecoration(
+          color: Color(0xffdef2f8), borderRadius: BorderRadius.circular(10)),
+      child: Stack(
+        children: [
+          Align(
+            // top: MediaQuery.of(context).size.height * 0.2 / 2.5,
+            alignment: Alignment.centerLeft,
+            child: Container(
+              width: MediaQuery.of(context).size.width * 0.45,
+              child: Text(
+                'Planks ona thte case the',
+                // overflow: TextOverflow.ellipsis,
+                style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.blueGrey),
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.centerRight,
+            child: Image(
+              image: AssetImage(imgUrl),
+              height: _height,
+              width: _width,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/onboarding.dart
+++ lib/onboarding.dart
@@ -52,9 +52,11 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
   Widget build(BuildContext context) {
     var _height = MediaQuery.of(context).size.height * 0.4;
     var _width = MediaQuery.of(context).size.width * 0.7;
-    return Scaffold(
-      body: SafeArea(
-        child: Container(
+    
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: SystemUiOverlayStyle.light,
+      child: Scaffold(
+        body: Container(
           decoration: BoxDecoration(
             gradient: LinearGradient(
               begin: Alignment.topCenter,
@@ -68,216 +70,217 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
               ],
             ),
           ),
-          child: Padding(
-            padding: EdgeInsets.symmetric(vertical: 5.0, horizontal: 8),
-            child: ListView(
-              children: <Widget>[
-                Container(
-                  padding: EdgeInsets.only(top: 5),
-                  alignment: Alignment.centerRight,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      _pageController.animateToPage(3,
-                          duration: Duration(milliseconds: 300),
-                          curve: Curves.bounceIn);
-                      setState(() {
-                        _currentPage = 3;
-                      });
-                    },
-                    style: ElevatedButton.styleFrom(
-                      padding: EdgeInsets.all(15),
-                      primary: Theme.of(context).primaryColor,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20)),
-                    ),
-                    child: Text(
-                      'Skip',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 18.0,
+          child: SafeArea(
+            child: Padding(
+              padding: EdgeInsets.symmetric(vertical: 5.0, horizontal: 8),
+              child: ListView(
+                children: <Widget>[
+                  Container(
+                    padding: EdgeInsets.only(top: 5),
+                    alignment: Alignment.centerRight,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        _pageController.animateToPage(3,
+                            duration: Duration(milliseconds: 300),
+                            curve: Curves.bounceIn);
+                        setState(() {
+                          _currentPage = 3;
+                        });
+                      },
+                      style: ElevatedButton.styleFrom(
+                        padding: EdgeInsets.all(15),
+                        primary: Theme.of(context).primaryColor,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20)),
+                      ),
+                      child: Text(
+                        'Skip',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18.0,
+                        ),
                       ),
                     ),
                   ),
-                ),
-                Container(
-                  height: MediaQuery.of(context).size.height * 0.7,
-                  child: PageView(
-                    physics: ClampingScrollPhysics(),
-                    controller: _pageController,
-                    onPageChanged: (int page) {
-                      setState(() {
-                        _currentPage = page;
-                      });
-                    },
-                    children: <Widget>[
-                      
-                      Padding(
-                        padding: EdgeInsets.all(20.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Center(
-                              child: Image(
-                                image: AssetImage(
-                                    'assets/images/healthy_lifestyle.png'),
-                                height: _height,
-                                width: _width,
-                              ),
-                            ),
-                            SizedBox(height: 30.0),
-                            Text(
-                              'Live a healthy life Sytle\nwith us!',
-                              style: kTitleStyle,
-                            ),
-                            SizedBox(height: 15.0),
-                            Text(
-                              'Lorem ipsum dolor sit amet, consect adipiscing elit, sed do eiusmod tempor incididunt ut labore et.',
-                              style: kSubtitleStyle,
-                            ),
-                          ],
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.all(20.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Center(
-                              child: Image(
-                                image: AssetImage(
-                                    'assets/images/fitness_tracker.png'),
-                                height: _height,
-                                width: _width,
-                              ),
-                            ),
-                            SizedBox(height: 10.0),
-                            Text(
-                              'Track Your fitness \nwith us',
-                              style: kTitleStyle,
-                            ),
-                            SizedBox(height: 8.0),
-                            Text(
-                              'Lorem ipsum dolor sit amet, consect adipiscing elit, sed do eiusmod tempor incididunt ut labore et.',
-                              style: kSubtitleStyle,
-                            ),
-                          ],
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.all(20.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Center(
-                              child: Image(
-                                image: AssetImage(
-                                    'assets/images/personal_trainer.png'),
-                                height: _height,
-                                width: _width,
-                              ),
-                            ),
-                            SizedBox(height: 10.0),
-                            Text(
-                              'Train with the Best',
-                              style: kTitleStyle,
-                            ),
-                            SizedBox(height: 8.0),
-                            Text(
-                              'Lorem ipsum dolor sit amet, consect adipiscing elit, sed do eiusmod tempor incididunt ut labore et.',
-                              style: kSubtitleStyle,
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: _buildPageIndicator(),
-                    ),
-                    SizedBox(width: 10),
-                    _currentPage != _numPages - 1
-                        ? SizedBox(
-                            child: Align(
-                              alignment: FractionalOffset.bottomRight,
-                              child: ElevatedButton(
-                                onPressed: () {
-                                  _pageController.nextPage(
-                                    duration: Duration(milliseconds: 500),
-                                    curve: Curves.ease,
-                                  );
-                                },
-                                style: ElevatedButton.styleFrom(
-                                  padding: EdgeInsets.all(15),
-                                  primary: Theme.of(context).primaryColor,
-                                  shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(20)),
+                  Container(
+                    height: MediaQuery.of(context).size.height * 0.7,
+                    child: PageView(
+                      physics: ClampingScrollPhysics(),
+                      controller: _pageController,
+                      onPageChanged: (int page) {
+                        setState(() {
+                          _currentPage = page;
+                        });
+                      },
+                      children: <Widget>[
+                        Padding(
+                          padding: EdgeInsets.all(20.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Center(
+                                child: Image(
+                                  image: AssetImage(
+                                      'assets/images/healthy_lifestyle.png'),
+                                  height: _height,
+                                  width: _width,
                                 ),
-                                child: Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  mainAxisSize: MainAxisSize.min,
-                                  children: <Widget>[
-                                    Text(
-                                      'Next',
-                                      style: TextStyle(
-                                        color: Colors.white,
-                                        fontSize: 22.0,
+                              ),
+                              SizedBox(height: 30.0),
+                              Text(
+                                'Live a healthy life Sytle\nwith us!',
+                                style: kTitleStyle,
+                              ),
+                              SizedBox(height: 15.0),
+                              Text(
+                                'Lorem ipsum dolor sit amet, consect adipiscing elit, sed do eiusmod tempor incididunt ut labore et.',
+                                style: kSubtitleStyle,
+                              ),
+                            ],
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.all(20.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Center(
+                                child: Image(
+                                  image: AssetImage(
+                                      'assets/images/fitness_tracker.png'),
+                                  height: _height,
+                                  width: _width,
+                                ),
+                              ),
+                              SizedBox(height: 10.0),
+                              Text(
+                                'Track Your fitness \nwith us',
+                                style: kTitleStyle,
+                              ),
+                              SizedBox(height: 8.0),
+                              Text(
+                                'Lorem ipsum dolor sit amet, consect adipiscing elit, sed do eiusmod tempor incididunt ut labore et.',
+                                style: kSubtitleStyle,
+                              ),
+                            ],
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.all(20.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Center(
+                                child: Image(
+                                  image: AssetImage(
+                                      'assets/images/personal_trainer.png'),
+                                  height: _height,
+                                  width: _width,
+                                ),
+                              ),
+                              SizedBox(height: 10.0),
+                              Text(
+                                'Train with the Best',
+                                style: kTitleStyle,
+                              ),
+                              SizedBox(height: 8.0),
+                              Text(
+                                'Lorem ipsum dolor sit amet, consect adipiscing elit, sed do eiusmod tempor incididunt ut labore et.',
+                                style: kSubtitleStyle,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: _buildPageIndicator(),
+                      ),
+                      SizedBox(width: 10),
+                      _currentPage != _numPages - 1
+                          ? SizedBox(
+                              child: Align(
+                                alignment: FractionalOffset.bottomRight,
+                                child: ElevatedButton(
+                                  onPressed: () {
+                                    _pageController.nextPage(
+                                      duration: Duration(milliseconds: 500),
+                                      curve: Curves.ease,
+                                    );
+                                  },
+                                  style: ElevatedButton.styleFrom(
+                                    padding: EdgeInsets.all(15),
+                                    primary: Theme.of(context).primaryColor,
+                                    shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(20)),
+                                  ),
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: <Widget>[
+                                      Text(
+                                        'Next',
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 22.0,
+                                        ),
                                       ),
-                                    ),
-                                    SizedBox(width: 10.0),
-                                    Icon(
-                                      Icons.arrow_forward,
-                                      color: Colors.white,
-                                      size: 30.0,
-                                    ),
-                                  ],
+                                      SizedBox(width: 10.0),
+                                      Icon(
+                                        Icons.arrow_forward,
+                                        color: Colors.white,
+                                        size: 30.0,
+                                      ),
+                                    ],
+                                  ),
                                 ),
                               ),
-                            ),
-                          )
-                        : Text(''),
-                  ],
-                )
-              ],
+                            )
+                          : Text(''),
+                    ],
+                  )
+                ],
+              ),
             ),
           ),
         ),
-      ),
-      bottomSheet: _currentPage == _numPages - 1
-          ? Container(
-              height: 80.0,
-              width: double.infinity,
-              color: Theme.of(context).primaryColor,
-              child: GestureDetector(
-                onTap: () async {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (BuildContext ctx) => Home()),
-                  );
-                  SharedPreferences prefs =
-                      await SharedPreferences.getInstance();
-                  prefs.setString('usagekey', 'times');
-                },
-                child: Center(
-                  child: Padding(
-                    padding: EdgeInsets.only(bottom: 30.0),
-                    child: Text(
-                      'Get started',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 28.0,
-                        fontWeight: FontWeight.bold,
+        bottomSheet: _currentPage == _numPages - 1
+            ? Container(
+                height: 80.0,
+                width: double.infinity,
+                color: Theme.of(context).primaryColor,
+                child: GestureDetector(
+                  onTap: () async {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (BuildContext ctx) => Home()),
+                    );
+                    SharedPreferences prefs =
+                        await SharedPreferences.getInstance();
+                    prefs.setString('usagekey', 'times');
+                  },
+                  child: Center(
+                    child: Padding(
+                      padding: EdgeInsets.only(bottom: 30.0),
+                      child: Text(
+                        'Get started',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 28.0,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
                     ),
                   ),
                 ),
-              ),
-            )
-          : Text(''),
+              )
+            : Text(''),
+      ),
     );
   }
 }
